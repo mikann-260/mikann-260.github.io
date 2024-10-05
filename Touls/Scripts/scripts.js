6401909e70a0ff24ef315ea5f2a7f3dd1a921d36
mikann-260 <181639134+mikann-260@users.noreply.github.com>
2024-10-05 15:22:51 +0900

    微調整

--- Touls/Scripts/scripts.js
+++ Touls/Scripts/scripts.js
@@ -10,11 +10,11 @@ function Question_Generation() {
   var selectText = document.getElementById("Issue_type").value;
   if (selectText !== "Select_type_of_issue") {
     if (selectText == "Area_of_circle") {
-      let Diameter_or_radius_List = ["直径","半径"]
+      let Diameter_or_radius_List = ["直径","半径"];
       let percentage_List = ["全体","2分の1","4分の1","8分の1"];
 
       var percentage = percentage_List[Math.floor(Math.random() * percentage_List.length)];
-      var size = getRandomInt(2,25);
+      var size = getRandomInt(2, 25);
       var Diameter_or_radius = Diameter_or_radius_List[Math.floor(Math.random() * Diameter_or_radius_List.length)];
 
       window.element = {
@@ -27,38 +27,36 @@ function Question_Generation() {
       document.getElementById("complementary").textContent = "(円周率は3.14とする)";
       document.getElementById("Answers_to_the_question").textContent = "";
     }
-  }
-  else {
-    document.getElementById("problem_statement").textContent = <tr>;
-    document.getElementById("complementary").textContent = <tr>;
+  } else {
+    document.getElementById("problem_statement").innerHTML = "<br>";
+    document.getElementById("complementary").innerHTML = "<br>";
   }
 }
 
 function answer() {
   var selectText = document.getElementById("Issue_type").value;
   if (selectText == "Area_of_circle") {
+    var radius;
     if (element["Diameter_or_radius"] == "直径") {
-      var radius = element["size"] / 2
-    }
-    else if (element["Diameter_or_radius"] == "半径") {
-      var radius = element["size"]
+      radius = element["size"] / 2;
+    } else if (element["Diameter_or_radius"] == "半径") {
+      radius = element["size"];
     }
 
+    var percentage;
     if (element["percentage"] == "全体") {
-      var percentage = 1
+      percentage = 1;
+    } else if (element["percentage"] == "2分の1") {
+      percentage = 1 / 2;
+    } else if (element["percentage"] == "4分の1") {
+      percentage = 1 / 4;
+    } else if (element["percentage"] == "8分の1") {
+      percentage = 1 / 8;
     }
-    else if (element["percentage"] == "2分の1") {
-      var percentage = 1 / 2
-    }
-    else if (element["percentage"] == "4分の1") {
-      var percentage = 1 / 4
-    }
-    else if (element["percentage"] == "8分の1") {
-      var percentage = 1 / 8
-    }
-  var Answer = (radius ** 2 * 3.14) * percentage
-  document.getElementById("Answers_to_the_question").textContent = `${Answer}㎠`;
-}
+
+    var Answer = (radius ** 2 * 3.14) * percentage;
+    document.getElementById("Answers_to_the_question").textContent = `${Answer}㎠`;
+  }
 }
 
 function hide() {
