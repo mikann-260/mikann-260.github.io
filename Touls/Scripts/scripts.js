34e005d1f022b3a3f405e5af2ad3bb2968eb52be
mikann-260 <181639134+mikann-260@users.noreply.github.com>
2024-10-05 12:07:33 +0900

    Update scripts.js

--- Touls/Scripts/scripts.js
+++ Touls/Scripts/scripts.js
@@ -2,7 +2,7 @@ function hideOption() {
   var select = document.getElementById("Issue_type");
   var optionToHide = select.querySelector('option[value="Select_type_of_issue"]');
   if (optionToHide) {
-      optionToHide.style.display = 'none';
+      optionToHide.style.display = none;
   }
 }
 
