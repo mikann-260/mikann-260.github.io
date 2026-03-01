dcfbbc8f596117472efed2ea2ada47c5dc42d8b8
mikann-260 <souma.260.math@gmail.com>
2026-03-01 22:36:45 +0900

    フォルダ構成を更新、計算ロジックの改善、UIデザインの調整、ファビコンの設定

--- tools/record-converter/script.js
+++ tools/record-converter/script.js
@@ -12,20 +12,26 @@ function calculate() {
   const varM = minutes.value;
   const varS = seconds.value;
   const varAL = afterLen.value;
-  if (varBL !== "" && varM !== "" && varS !== "") {
-    const bL = parseFloat(varBL);
-    const m = parseFloat(varM);
-    const s = parseFloat(varS);
-    const time = m * 60 + s;
-    hm.textContent = `時速: ${((bL / time) * 3.6).toFixed(2)}km/h`;
-    mm.textContent = `分速: ${((bL / time) * 60).toFixed(2)}m/m`;
-    sm.textContent = `秒速: ${(bL / time).toFixed(2)}m/s`;
-    if (varAL !== "") {
-      const aL = parseFloat(varAL);
-      const newTime = time * (aL / bL);
-      const newMinute = Math.floor(newTime / 60);
-      const newSecond = (newTime % 60).toFixed(0);
-      output.textContent = `変換後のタイム: ${aL}m: ${newMinute}分${newSecond}秒`;
+  if (varS !== "") {
+    let s = parseFloat(varS);
+    if (60 <= s) {
+      seconds.value = 59;
+      s = 59;
+    }
+    if (varBL !== "" && varM !== "") {
+      const bL = parseFloat(varBL);
+      const m = parseFloat(varM);
+      const time = m * 60 + s;
+      hm.textContent = `時速: ${((bL / time) * 3.6).toFixed(2)}km/h`;
+      mm.textContent = `分速: ${((bL / time) * 60).toFixed(2)}m/m`;
+      sm.textContent = `秒速: ${(bL / time).toFixed(2)}m/s`;
+      if (varAL !== "") {
+        const aL = parseFloat(varAL);
+        const newTime = time * (aL / bL);
+        const newMinute = Math.floor(newTime / 60);
+        const newSecond = (newTime % 60).toFixed(0);
+        output.textContent = `変換後のタイム: ${aL}m: ${newMinute}分${newSecond}秒`;
+      }
     }
   } else {
     output.textContent = `変換後のタイム: `;
