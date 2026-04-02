ce1edbfc28dd03bb3692b44183252b81aaaa4636
mikann-260 <souma.260.math@gmail.com>
2026-04-02 13:41:49 +0900

    feat: Cloudflareのアクセス解析をパラメータで無効化可能に

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
 document.getElementById("js-enabled").hidden = false;
+
+const params = new URLSearchParams(window.location.search);
+const disable = params.has("disable");
+
+if (!disable) {
+  const script = document.createElement("script");
+  script.src = "https://static.cloudflareinsights.com/beacon.min.js";
+  script.defer = true;
+  script.setAttribute(
+    "data-cf-beacon",
+    '{"token": "9e707a26d8b34fc5a82d3bb5afeba95b"}',
+  );
+  document.head.appendChild(script);
+}
